956e3f775af4376b917f05d91a72dba6b725fbd2
fmallouli <fmallouli@ACS45.ACS.LOCAL>
2019-01-23 10:10:23 +0100

    creation des objets

---
+++ src/main/java/com/agencevoyage/microservice/bean/Omra.java
@@ -0,0 +1,5 @@
+package com.agencevoyage.microservice.bean;
+
+public class Omra extends Produit{
+
+}
---
+++ src/main/java/com/agencevoyage/microservice/bean/Produit.java
@@ -0,0 +1,8 @@
+package com.agencevoyage.microservice.bean;
+
+import java.math.BigDecimal;
+
+public class Produit {
+
+	protected BigDecimal prix;
+}
